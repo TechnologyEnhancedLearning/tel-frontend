95a3328629d7fd9ea8d488707d9e4cd8b3c9061a
Paul Steven <114067294+paulstevendev@users.noreply.github.com>
2025-09-30 14:53:08 +0100

    Update hello-world.js

--- packages/tel-frontend/src/tel/components/hello-world/hello-world.js
+++ packages/tel-frontend/src/tel/components/hello-world/hello-world.js
@@ -1,25 +1,21 @@
 // packages/tel-frontend/src/components/hello-world/hello-world.js
 
 /**
- * Hello World component JS
- * This is where you add interactive behaviour for your component.
- * 
- * Example: Highlight the component when it is clicked.
+ * HelloWorld component
+ * Example: Highlight the component when clicked
  */
 
-function HelloWorld($module) {
-  this.$module = $module;
-}
-
-HelloWorld.prototype.init = function () {
-  if (!this.$module) {
-    return;
+export default class HelloWorld {
+  constructor($module) {
+    this.$module = $module;
   }
 
-  // Example behaviour: toggle a highlight class on click
-  this.$module.addEventListener('click', () => {
-    this.$module.classList.toggle('hello-world--highlight');
-  });
-};
+  init() {
+    if (!this.$module) return;
 
-export default HelloWorld;
+    // Example behaviour: toggle highlight class on click
+    this.$module.addEventListener('click', () => {
+      this.$module.classList.toggle('hello-world--highlight');
+    });
+  }
+}
